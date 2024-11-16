c87517840c9ec2a3fe2dff927412dc394f32c489
Ryu Satoy <dan_ryuichi@outlook.com>
2024-11-16 01:25:53 -0300

    chamada de função com parâmetros

--- substituiExtensao.sh
+++ substituiExtensao.sh
@@ -1,36 +1,32 @@
 #!/bin/bash
-
 Principal() {
-  clear
-  read -p "Digite o diretório (desde a raiz) que tem os arquivos que deseja: " dirAlvo
+  if [ $# != 3 ]; then
+    echo "Esse método deve ter três parâmetros, o primeiro é o diretório alvo, cujo os arquivos que deseja estão presentes;"
+    echo "O segundo, deve ser a extensão (sem ponto) que deseja remover"
+    echo "O terceiro, deve ser a extensão (sem ponto) da qual deseja que o seu arquivo receba"
+    sleep 3; exit
+  fi
 
-  if [ -z "$dirAlvo" ]; then
+  if [ -z "$1" ]; then
     echo "Você não informou o nome!"; sleep 2
-    Principal
+    Principal "$1" "$2" "$3"
   else
-    if [ -d "$dirAlvo" ]; then
-      cd "$dirAlvo" || { echo "Falha ao mudar para o diretório"; exit 1; }
-      substituir
+    if [ -d "$1" ]; then
+      cd "$1" || { echo "Falha ao mudar para o diretório"; exit 1; }
+      arquivos=0
+        for arq in *."$2"; do
+          mv "$arq" "${arq%.$2}.$3" && ((arquivos++)) 
+        done
+
+        if [[ $arquivos -gt 0 ]]; then
+          echo "$arquivos arquivos renomeados com sucesso!"
+        else
+          echo "Nenhum arquivo encontrado com a extensão $2."
+        fi
     else
       echo "Não é um diretório!"; sleep 2
-      Principal
+      Principal "$1" "$2" "$3"
     fi
   fi
 }
-
-substituir() {
-  read -p "Digite a extensão que deseja tirar e também a extensão que deseja colocar no lugar, respectivamente e sem ponto: " tirar colocar
-
-  arquivos=0
-  for arq in *."$tirar"; do
-    mv "$arq" "${arq%.$tirar}.$colocar" && ((arquivos++))
-  done
-
-  if [[ $arquivos -gt 0 ]]; then
-    echo "$arquivos arquivos renomeados com sucesso!"
-  else
-    echo "Nenhum arquivo encontrado com a extensão '$tirar'."
-  fi
-}
-
-Principal
+Principal "$@"
